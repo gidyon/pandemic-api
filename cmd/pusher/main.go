89f1cc1dd80899012b7db31ae80b456cec429173
Gideon Kamau <gideon.ngk@gmail.com>
2020-05-06 10:37:01 +0300

    pusher service

---
+++ cmd/pusher/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/Sirupsen/logrus"
+	"github.com/appleboy/go-fcm"
+	"github.com/gidyon/config"
+	"github.com/gidyon/micros"
+	"github.com/gidyon/pandemic-api/internal/pusher"
+	"os"
+)
+
+var interval = flag.Int("interval-minutes", 5, "interval to request for location")
+
+func main() {
+	ctx := context.Background()
+
+	cfg, err := config.New()
+	handleErr(err)
+
+	service, err := micros.NewService(context.Background(), cfg, micros.NewLogger("pusher"))
+	handleErr(err)
+
+	// FCM client
+	fcmClient, err := fcm.NewClient(os.Getenv("FCM_SERVER_KEY"))
+	handleErr(err)
+
+	// Starts pusher
+	err = pusher.StartWorker(ctx, &pusher.Options{
+		SQLDB:     service.GormDB(),
+		RedisDB:   service.RedisClient(),
+		FCMClient: fcmClient,
+		Logger:    service.Logger(),
+		Interval:  *interval,
+	})
+	handleErr(err)
+}
+
+func handleErr(err error) {
+	if err != nil {
+		logrus.Fatalln(err)
+	}
+}
